13332fd821a9294016ab13589b2ea622bbcdab95
unknown <thuannmph19038@nnmph19038@fpt.edu.vn>
2022-07-16 12:43:17 +0700

    update footer

---
+++ footer.php
@@ -0,0 +1,64 @@
+<footer class="bg-dark">
+    <div class="container">
+        <div class="footer-top">
+            <div class="row">
+                <div class="col-md-3">
+                    <h4>Tổng đài</h4>
+                    <a href="">
+                        <p class="text-light">Mua hàng: 1900.9696.42 (7:30 - 22:00)</p>
+                    </a><a href="">
+                        <p class="text-light">CSKH: 1900.9868.43 (8:00 - 21:30)</p>
+                    </a><a href="">
+                        <p class="text-light">Kỹ thuật: 1900.8668.54 (7:30 - 22:00)</p>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <h4>Hệ thống cửa hàng</h4>
+                    <a href="">
+                        <p class="text-light">Nội quy cửa hàng</p>
+                    </a><a href="">
+                        <p class="text-light">Chất lượng phục vụ</p>
+                    </a><a href="">
+                        <p class="text-light">Chính sách bảo hành & đổi trả</p>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <h4>Hỗ trợ khách hàng</h4>
+                    <a href="">
+                        <p class="text-light">Điều kiện giao dịch chung</p>
+                    </a><a href="">
+                        <p class="text-light">Hướng dẫn mua hàng online</p>
+                    </a><a href="">
+                        <p class="text-light">Chính sách giao hàng</p>
+                    </a><a href="">
+                        <p class="text-light">Hướng dẫn thanh toán</p>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <h4>Thương hiệu</h4>
+                    <a href="">
+                        <p class="text-light">Hướng dẫn mua hàng online</p>
+                    </a><a href="">
+                        <p class="text-light">Chính sách giao hàng</p>
+                    </a><a href="">
+                        <p class="text-light">Hướng dẫn thanh toán</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-end">
+            <div class="row">
+                <div class="col-md-8">
+                    <p class="text-light">© 2018. Công ty cổ phần Thế Giới Alo. GPDKKD: 0303217354 do sở KH & ĐT TP.HCM cấp ngày 02/01/2022</p>
+                    <p class="text-light">Địa chỉ: 128 Trần Quang Khải, P. Tân Định, Q.1, TP. Hồ Chí Minh. Điện thoại: 028 38125960.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="./image/image 1.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
