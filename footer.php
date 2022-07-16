215d5ec9d9f73aa4d5eabfeb0403e413eb73ed03
unknown <thuannmph19038@nnmph19038@fpt.edu.vn>
2022-07-16 12:53:57 +0700

    upload footer

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 <footer class="bg-dark">
     <div class="container">
         <div class="footer-top">
             <div class="row">
                 <div class="col-md-3">
-                    <h4>Tổng đài</h4>
+                    <h4 class="text-light">Tổng đài</h4>
                     <a href="">
                         <p class="text-light">Mua hàng: 1900.9696.42 (7:30 - 22:00)</p>
                     </a><a href="">
@@ -14,7 +13,7 @@
                     </a>
                 </div>
                 <div class="col-md-3">
-                    <h4>Hệ thống cửa hàng</h4>
+                    <h4 class="text-light">Hệ thống cửa hàng</h4>
                     <a href="">
                         <p class="text-light">Nội quy cửa hàng</p>
                     </a><a href="">
@@ -24,7 +23,7 @@
                     </a>
                 </div>
                 <div class="col-md-3">
-                    <h4>Hỗ trợ khách hàng</h4>
+                    <h4 class="text-light">Hỗ trợ khách hàng</h4>
                     <a href="">
                         <p class="text-light">Điều kiện giao dịch chung</p>
                     </a><a href="">
@@ -36,7 +35,7 @@
                     </a>
                 </div>
                 <div class="col-md-3">
-                    <h4>Thương hiệu</h4>
+                    <h4 class="text-light">Thương hiệu</h4>
                     <a href="">
                         <p class="text-light">Hướng dẫn mua hàng online</p>
                     </a><a href="">
@@ -49,11 +48,11 @@
         </div>
         <div class="footer-end">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-3">
                     <p class="text-light">© 2018. Công ty cổ phần Thế Giới Alo. GPDKKD: 0303217354 do sở KH & ĐT TP.HCM cấp ngày 02/01/2022</p>
                     <p class="text-light">Địa chỉ: 128 Trần Quang Khải, P. Tân Định, Q.1, TP. Hồ Chí Minh. Điện thoại: 028 38125960.</p>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <img src="./image/image 1.png" alt="">
                 </div>
             </div>
