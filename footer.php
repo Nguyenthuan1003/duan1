56912858cf64143fe4980c1c8c62e2123ee83dde
unknown <thuannmph19038@nnmph19038@fpt.edu.vn>
2022-07-16 12:58:29 +0700

    update footer

--- footer.php
+++ footer.php
@@ -1,56 +1,56 @@
-<footer class="bg-dark">
+<footer class="bg-dark p-5">
     <div class="container">
         <div class="footer-top">
             <div class="row">
                 <div class="col-md-3">
-                    <h4 class="text-light">Tổng đài</h4>
+                    <h4 class="text-light p-1">Tổng đài</h4>
                     <a href="">
-                        <p class="text-light">Mua hàng: 1900.9696.42 (7:30 - 22:00)</p>
+                        <p class="text-light p-1">Mua hàng: 1900.9696.42 (7:30 - 22:00)</p>
                     </a><a href="">
-                        <p class="text-light">CSKH: 1900.9868.43 (8:00 - 21:30)</p>
+                        <p class="text-light p-1">CSKH: 1900.9868.43 (8:00 - 21:30)</p>
                     </a><a href="">
-                        <p class="text-light">Kỹ thuật: 1900.8668.54 (7:30 - 22:00)</p>
+                        <p class="text-light p-1">Kỹ thuật: 1900.8668.54 (7:30 - 22:00)</p>
                     </a>
                 </div>
                 <div class="col-md-3">
-                    <h4 class="text-light">Hệ thống cửa hàng</h4>
+                    <h4 class="text-light p-1">Hệ thống cửa hàng</h4>
                     <a href="">
-                        <p class="text-light">Nội quy cửa hàng</p>
+                        <p class="text-light p-1">Nội quy cửa hàng</p>
                     </a><a href="">
-                        <p class="text-light">Chất lượng phục vụ</p>
+                        <p class="text-light p-1">Chất lượng phục vụ</p>
                     </a><a href="">
-                        <p class="text-light">Chính sách bảo hành & đổi trả</p>
+                        <p class="text-light p-1">Chính sách bảo hành & đổi trả</p>
                     </a>
                 </div>
                 <div class="col-md-3">
-                    <h4 class="text-light">Hỗ trợ khách hàng</h4>
+                    <h4 class="text-light p-1">Hỗ trợ khách hàng</h4>
                     <a href="">
-                        <p class="text-light">Điều kiện giao dịch chung</p>
+                        <p class="text-light p-1">Điều kiện giao dịch chung</p>
                     </a><a href="">
-                        <p class="text-light">Hướng dẫn mua hàng online</p>
+                        <p class="text-light p-1">Hướng dẫn mua hàng online</p>
                     </a><a href="">
-                        <p class="text-light">Chính sách giao hàng</p>
+                        <p class="text-light p-1">Chính sách giao hàng</p>
                     </a><a href="">
-                        <p class="text-light">Hướng dẫn thanh toán</p>
+                        <p class="text-light p-1">Hướng dẫn thanh toán</p>
                     </a>
                 </div>
                 <div class="col-md-3">
-                    <h4 class="text-light">Thương hiệu</h4>
+                    <h4 class="text-light p-1">Thương hiệu</h4>
                     <a href="">
-                        <p class="text-light">Hướng dẫn mua hàng online</p>
+                        <p class="text-light p-1">Hướng dẫn mua hàng online</p>
                     </a><a href="">
-                        <p class="text-light">Chính sách giao hàng</p>
+                        <p class="text-light p-1">Chính sách giao hàng</p>
                     </a><a href="">
-                        <p class="text-light">Hướng dẫn thanh toán</p>
+                        <p class="text-light p-1">Hướng dẫn thanh toán</p>
                     </a>
                 </div>
             </div>
         </div>
         <div class="footer-end">
             <div class="row">
-                <div class="col-md-3">
-                    <p class="text-light">© 2018. Công ty cổ phần Thế Giới Alo. GPDKKD: 0303217354 do sở KH & ĐT TP.HCM cấp ngày 02/01/2022</p>
-                    <p class="text-light">Địa chỉ: 128 Trần Quang Khải, P. Tân Định, Q.1, TP. Hồ Chí Minh. Điện thoại: 028 38125960.</p>
+                <div class="col-md-9">
+                    <p class="text-light p-1">© 2018. Công ty cổ phần Thế Giới Alo. GPDKKD: 0303217354 do sở KH & ĐT TP.HCM cấp ngày 02/01/2022</p>
+                    <p class="text-light p-1">Địa chỉ: 128 Trần Quang Khải, P. Tân Định, Q.1, TP. Hồ Chí Minh. Điện thoại: 028 38125960.</p>
                 </div>
                 <div class="col-md-3">
                     <img src="./image/image 1.png" alt="">
