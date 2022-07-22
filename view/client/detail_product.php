ebcaf7201ab7869ab8bf5c4927cee78aa027cded
Ngapt <ngaptph19327@fpt.edu.vn>
2022-07-22 10:06:12 +0700

    detail_product

---
+++ view/client/detail_product.php
@@ -0,0 +1,236 @@
+<main>
+<div class="container">
+    <div class="row" id="row">
+      <div class="col-md-6" id="colum-one">
+        <div class="img">
+          <img src="https://cf.shopee.vn/file/7e07f670fe59ab60d9342f737202f026" alt="" class="img-product-1">
+          <div class="icon-one">
+            <a href=""><i class="fa-solid fa-arrow-left-long icon text-icon "></i></a>
+          </div>
+          <div class="icon-tow">
+            <a href=""> <i class="fa-solid fa-arrow-right-long icon text-icon "></i></a>
+          </div>
+
+        </div>
+        <div class="img-details">
+          <div class="img-details-1">
+            <a href="">
+              <img
+                src="https://media.baodansinh.vn/baodansinh/2019/10/6/images23715081appleiphone11pro-1570328286194-1570328286195784742171.jpg"
+                alt="" class="img-detail-1">
+            </a>
+          </div>
+          <div class="img-details-1">
+            <a href="">
+              <img
+                src="https://media.baodansinh.vn/baodansinh/2019/10/6/images23715081appleiphone11pro-1570328286194-1570328286195784742171.jpg"
+                alt="" class="img-detail-1">
+            </a>
+          </div>
+          <div class="img-details-1">
+            <a href="">
+              <img
+                src="https://media.baodansinh.vn/baodansinh/2019/10/6/images23715081appleiphone11pro-1570328286194-1570328286195784742171.jpg"
+                alt="" class="img-detail-1">
+            </a>
+          </div>
+          <div class="img-details-1">
+            <a href="">
+              <img
+                src="https://media.baodansinh.vn/baodansinh/2019/10/6/images23715081appleiphone11pro-1570328286194-1570328286195784742171.jpg"
+                alt="" class="img-detail-1">
+            </a>
+          </div>
+          <div class="img-details-1">
+            <a href="">
+              <img
+                src="https://media.baodansinh.vn/baodansinh/2019/10/6/images23715081appleiphone11pro-1570328286194-1570328286195784742171.jpg"
+                alt="" class="img-detail-1">
+            </a>
+          </div>
+
+
+
+
+
+
+
+        </div>
+
+
+      </div>
+      <div class="col-md-6">
+        <h2 class="text-1">iPhone 13 Pro Max</h2>
+        <div class="variant">
+          <div class="variant-price">
+            <span class="price_new text-1">27.790.000₫ </span>
+            <span class="price_old text-1">33.990.000₫</span>
+            <span class="discount text-1">-18%</span>
+            <h4 class="text-1">Dung lượng</h4>
+            <div class="variant-Capacity">
+              <button class=" btn btn-secondary text-1">128GB</button>
+              <button class=" btn btn-secondary text-1">256GB</button>
+              <button class=" btn btn-secondary text-1">512GB</button>
+              <button class=" btn btn-secondary text-1">1TB</button>
+            </div>
+            <div class="variant-color">
+              <div class="text-color">
+                <span class="text-1">Màu:</span>
+                <span class="text-1">vàng đồng</span>
+              </div>
+              <div class="color">
+                <a href="">
+                  <div class="color-background color-1"></div>
+                </a>
+                <a href="">
+                  <div class="color-background color-2"></div>
+                </a>
+                <a href="">
+                  <div class="color-background color-3"></div>
+                </a>
+                <a href="">
+                  <div class="color-background color-4"></div>
+                </a>
+                <a href="">
+                  <div class="color-background color-5"></div>
+                </a>
+
+              </div>
+              <hr class=".hr">
+              <div class="bank">
+                <h3 class="text-1">Ưu đãi khi thanh toán</h3>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="VNPAY_bANK" id="bank-row">
+                      <div class="check-bank">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQYA6AqTFalZgwS4n6LuRbdr7GEZ-1yiuZQgHPKM6uunvGzMuXxaXVPYCaecUSv62zFQc&usqp=CAU"
+                          alt="" class="img-bank">
+
+                      </div>
+                      <div class="endow">
+                        <span>Giảm</span>
+                        <span>500.000₫</span>
+                      </div>
+                      <p class="tex-bank">Sản phẩm iPhone</p>
+
+                    </div>
+
+                  </div>
+                  <div class="col-md-6">
+                    <div class="EMXIBANK" id="bank-row">
+                      <div class="ckeck-bank">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        <img src="https://eximbank.com.vn/web/image/1198/LogoEximbank3.jpg" alt="" class="img-bank">
+
+                      </div>
+                      <div class="endow">
+                        <span>Giảm</span>
+                        <span>500.000₫</span>
+                      </div>
+                      <p class="tex-bank">Sản phẩm từ 3Tr</p>
+                    </div>
+                  </div>
+
+
+                </div>
+
+
+              </div>
+              <div class="d-grid gap-2 col-6 mx-auto " id="button">
+                <button class="btn btn-primary" id="margin" type="button">Mua ngay</button>
+              </div>
+              <div class="text">
+                <p class="text-1">Bộ sản phẩm gồm: Hộp, Sách hướng dẫn, Cây lấy sim, Cáp Lightning - Type C</p>
+                <p class="text-1">Hư gì đổi nấy 12 tháng tại 3253 siêu thị trên toàn quốc</p>
+                <a href="">Xem chi tiết chính sách bảo hành, đổi trả</a>
+                <p class="text-1">Bảo hành chính hãng 1 năm</p>
+                <span class="text-1">Bảo hành chính hãng 1 năm</span>
+                <a href="">Xem chi tiết</a>
+                <span class="text-1">Tổng đài:</span>
+                <a href="">1900.9696.42</a>
+                <span class="text-1">(9h00 - 21h00 mỗi ngày)</span>
+
+              </div>
+
+
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="product">
+      <h2>Phụ kiện gợi ý cho iPhone</h2>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="img">
+            <a href="">
+              <img src="https://img.websosanh.vn/v2/users/review/images/g0uimocg53p63.jpg?compress=85" alt=""
+                class="img-product">
+            </a>
+          </div>
+          <h3 class="title-product">Ốp lưng Nhựa dẻo iPhone 13 Pro Max Sạc MagSafe</h3>
+          <div class="price-product">
+            <span class="price-new-product">950.000₫</span>
+            <span class="price-old-product">950.000₫</span>
+            <span class="discount-product">-40%</span>
+          </div>
+
+        </div>
+        <div class="col-md-3">
+          <div class="img">
+            <a href="">
+              <img src="https://img.websosanh.vn/v2/users/review/images/g0uimocg53p63.jpg?compress=85" alt=""
+                class="img-product">
+            </a>
+          </div>
+          <h3 class="title-product">Sạc không dây Apple MagSafe Duo Charger</h3>
+          <div class="price-product">
+            <span class="price-new-product">2.990.000₫</span>
+            <span class="price-old-product">3.990.000₫</span>
+            <span class="discount-product">-25%</span>
+          </div>
+
+        </div>
+        <div class="col-md-3">
+          <div class="img">
+            <a href="">
+              <img src="https://img.websosanh.vn/v2/users/review/images/g0uimocg53p63.jpg?compress=85" alt=""
+                class="img-product">
+            </a>
+          </div>
+
+          <h3 class="title-product">Pin sạc dự phòng Apple MagSafe Battery Pack</h3>
+          <div class="price-product">
+            <span class="price-new-product">2.390.000₫</span>
+            <span class="price-old-product">2.990.000₫</span>
+            <span class="discount-product">-20%</span>
+          </div>
+
+        </div>
+        <div class="col-md-3">
+          <div class="img">
+            <a href="">
+              <img src="https://img.websosanh.vn/v2/users/review/images/g0uimocg53p63.jpg?compress=85" alt=""
+                class="img-product">
+            </a>
+
+          </div>
+          <h3 class="title-product">Cáp Type C - Lightning MFI Belkin Duratex Plus F8J243 1.2m</h3>
+          <div class="price-product">
+            <span class="price-new-product">560.000₫</span>
+            <span class="price-old-product">800.000₫</span>
+            <span class="discount-product">-30%</span>
+          </div>
+
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+</main>
