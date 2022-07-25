e9326687a1d417cb8eff7ba7ad17fd22f428545e
nguyenquangminh2001abc <minhnqph19074@fpt.edu.vn>
2022-07-25 21:23:32 +0700

    Update login.php

--- view/client/login.php
+++ view/client/login.php
@@ -1,6 +1,6 @@
-<main>
+<main class="bg-body">
     <div class="container p-5">
-        <div class="row">
+        <div class="row shadow-lg p-3 mb-5 bg-body rounded">
             <div class="col-md-6">
                 <img src="./image/banner_login.jpg" alt="ảnh" style="max-width:100%">
             </div>
@@ -16,14 +16,14 @@
                         <p class="text-primary text-start">Mật khẩu</p>
                         <input type="text"  class="form-control" name="password" placeholder="Nhập mật khẩu để đăng nhập">
                     </div>
-                    <input type="submit" value="Đăng nhập" class="btn btn-primary mt-4" name="login">
+                    <input type="submit" value="Đăng nhập" class="btn btn-primary mt-4 w-100" name="login">
                 </form>
                 <div class="btn-login">
                     <p class="text-secondary mt-3">hoặc</p>
-                    <a href=""><input type="submit" value="Đăng ký" class="btn btn-outline-primary"></a>
+                    <a href=""><input type="submit" value="Đăng ký" class="btn btn-outline-primary w-100"></a>
                     <div class="p-3">
-                        <a href="index.php" class="text-start me-2"><span>Quay về trang chủ</span></a>
-                        <a href=""><span class="text-end">Quên mật khẩu</span></a>
+                        <a href="index.php" class="text-start me-4"><span>Quay về trang chủ</span></a>
+                        <a href=""><span class="text-end">Quên mật khẩu?</span></a>
                     </div>
                 </div>
             </div>
