568575ea9f4f2a773e6d4cea27244f0d577851c6
nguyenquangminh2001abc <minhnqph19074@fpt.edu.vn>
2022-07-28 10:02:14 +0700

    Create recharge.php

---
+++ view/client/recharge.php
@@ -0,0 +1,101 @@
+<main>
+    <div class="row mt-4 m-auto">
+        <aside class="col-3">
+            <section class="border bg-primary text-white rounded text-center p-4">
+                <img src="./image/wallet.png" alt="">
+                <h2>Số Dư</h2>
+                <p>220,000 mbcoin</p>
+                <p>Tỷ Giá Hiện Tại 1 mbcoin = 1 vnđ</p>
+            </section>
+            <section class="border rounded mt-3 p-3">
+                <p>chú ý :</p>
+                <p>Hệ thống nạp tự động sẽ tự động cộng tiền vào tài khoản của quý khách.</p>
+                <p>Không mua bán trao đổi tài khoản và xu hệ thống dưới mọi hình thức.</p>
+            </section>
+        </aside>
+        <article class="col-8">
+            <section class="border rounded p-4">
+                <h2 class="text-primary">Bank</h2>
+                <hr class="text-primary">
+                <p>Cách nạp tiền : </p>
+                <p>Bước 1: Nội dung chuyển khoản</p>
+                <p>Bước 2: Tiến hành chuyển khoản</p>
+                <div>
+                    <article>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="row">Thông Tin Chuyển Khoản</th>
+                                    <td></td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Số Tài Khoản</th>
+                                    <td>0381000432664</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Chủ tài khoản</th>
+                                    <td>Nguyễn Quang Minh</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Nội dung chuyển khoản</th>
+                                    <td>mb22192010</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Chi nhánh</th>
+                                    <td>Vietcombank Chi Nhánh Hà Nội</td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </article>
+                </div>
+                <div class="alert alert-success" role="alert">
+                    Vui lòng chuyển khoản theo nội dung trên, hệ thống sẽ tự động cộng tiền sau khi bạn chuyển khoản
+                    thành công
+                </div>
+                <div class="alert alert-warning" role="alert">
+                    Nếu Không Thấy Cộng Tiền Vào Tài Khoản Vui Lòng Liên Hệ Với Admin
+                </div>
+            </section>
+            <section class="border rounded mt-3 p-4">
+            <h2 class="text-primary">Lịch Sử Nạp Tiền</h2>
+                <hr class="text-primary">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">STT</th>
+                            <th scope="col">Số Dư Trước Giao Dịch</th>
+                            <th scope="col">Mbcoin</th>
+                            <th scope="col">Nội Dung</th>
+                            <th scope="col">Trạng Thái</th>
+                            <th scope="col">Ngày Nạp Tiền</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>250,000</td>
+                            <td>250,000</td>
+                            <td>mb22192010</td>
+                            <td>Hoàn thành</td>
+                            <td>16:13 08/07/2022</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>250,000</td>
+                            <td>250,000</td>
+                            <td>mb22192010</td>
+                            <td>Hoàn thành</td>
+                            <td>16:13 08/07/2022</td>
+                        </tr>
+
+                        
+                    </tbody>
+                </table>
+            </section>
+        </article>
+
+    </div>
+</main>
