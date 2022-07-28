256431e2689067b80dae8c4fc333f169461bbe40
nguyenquangminh2001abc <minhnqph19074@fpt.edu.vn>
2022-07-28 10:02:08 +0700

    Update cart.php

--- view/client/cart.php
+++ view/client/cart.php
@@ -12,16 +12,21 @@
                         <article class="wrap_info_product_on_cart col-6">
                             <p class="name_product_on_cart">iPhone 13 Pro 256GB Chính Hãng (VN/A)</p>
                             <p class="color_product_on_cart btn btn-outline-primary btn-sm rounder">màu xanh</p>
-                        </article>
+                            </article>
                     </div>
                     <div class="total_price_product_on_cart col-6 d-flex">
 
                         <div class="buttons_added col-3   m-auto">
-                            <p class="price_product_on_cart text-danger mt-2 me-5">2200000đ</p>
-                            <p class="price_product_default" hidden >2200000đ</p>
-                            <input class="minus minus_product_on_cart" type="button" value="-" id="Subtraction">
+                            <p class="price_product_on_cart text-danger mt-2 me-5">2200000</p>
+                            <input class="price_product_default" hidden value="2200000">
+                            <!-- <input class="minus minus_product_on_cart" type="button" value="-" id="Subtraction">
                             <input class="quantity_product_on_cart p-0 text-center" type="number" value="1">
-                            <input class="plus plus_product_on_cart" type="button" value="+" id="plus">
+                            <input class="plus plus_product_on_cart" type="button" value="+" id="plus"> -->
+                            <div class="buttons_added">
+                                <input class="minus is-form" type="button" value="-" id="Subtraction">
+                                <input aria-label="quantity" class="input-qty" max="100000" min="1" name="" type="number" value="1" id="sum">
+                                <input class="plus is-form" type="button" value="+" id="plus">
+                            </div>
                             <a href="" class="text-danger ms-3 mt-2">xóa</a>
                         </div>
                     </div>
@@ -42,36 +47,16 @@
                     <div class="total_price_product_on_cart col-6 d-flex">
 
                         <div class="buttons_added col-3   m-auto">
-                            <p class="price_product_on_cart text-danger mt-2 me-5">2200000đ</p>
-                            <p class="price_product_default" hidden>2200000đ</p>
-                            <input class="minus minus_product_on_cart" type="button" value="-" id="Subtraction">
+                            <p class="price_product_on_cart text-danger mt-2 me-5">2200000</p>
+                            <input class="price_product_default" hidden value="2200000">
+                            <!-- <input class="minus minus_product_on_cart" type="button" value="-" id="Subtraction">
                             <input class="quantity_product_on_cart p-0 text-center" type="number" value="1">
-                            <input class="plus plus_product_on_cart" type="button" value="+" id="plus">
-                            <a href="" class="text-danger ms-3 mt-2">xóa</a>
-                        </div>
-                    </div>
-                </section>
-            </article>
-            <hr class="text-secondary">
-            <article class="wrap_product_on_cart">
-                <section class="product_on_cart row">
-                    <div class="info_product_on_cart col-6 row">
-                        <article class="img_product_on_cart  text-align-center d-flex col-5 ">
-                            <img src="./image/shopping (1).png" width="100px" class="m-auto" alt="">
-                        </article>
-                        <article class="wrap_info_product_on_cart col-6">
-                            <p class="name_product_on_cart">iPhone 13 Pro 256GB Chính Hãng (VN/A)</p>
-                            <p class="color_product_on_cart btn btn-outline-primary btn-sm rounder">màu xanh</p>
-                        </article>
-                    </div>
-                    <div class="total_price_product_on_cart col-6 d-flex">
-
-                        <div class="buttons_added col-3   m-auto">
-                            <p class="price_product_on_cart text-danger mt-2 me-5">2200000đ</p>
-                            <p class="price_product_default" hidden>2200000đ</p>
-                            <input class="minus minus_product_on_cart" type="button" value="-" id="Subtraction">
-                            <input class="quantity_product_on_cart p-0 text-center" type="number" value="1">
-                            <input class="plus plus_product_on_cart" type="button" value="+" id="plus">
+                            <input class="plus plus_product_on_cart" type="button" value="+" id="plus"> -->
+                            <div class="buttons_added">
+                                <input class="minus is-form Subtraction" type="button" value="-">
+<input aria-label="quantity" class="input-qty" max="100000" min="1" name="" type="number" value="1" id="sum">
+                                <input class="plus is-form" type="button" value="+" id="plus">
+                            </div>
                             <a href="" class="text-danger ms-3 mt-2">xóa</a>
                         </div>
                     </div>
@@ -105,4 +90,5 @@
         </section>
     </div>
 </article>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="./js/cart.js"></script>
