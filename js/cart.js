ca5bb3e6bcc327ae6e02a9f6fcd861bf90dca6ad
nguyenquangminh2001abc <minhnqph19074@fpt.edu.vn>
2022-07-28 10:02:06 +0700

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,57 @@
-var price_product = document.querySelectorAll(".price_product_on_cart");
-var quantity_product = document.querySelectorAll(".quantity_product_on_cart");
-var minus_product = document.querySelectorAll(".minus_product_on_cart");
-var plus_product = document.querySelectorAll(".plus_product_on_cart");
-var price_default = document.querySelectorAll(".price_product_default");
+$('input.input-qty').each(function() {
+  var $this = $(this),
+    qty = $this.parent().find('.is-form'),
+    min = Number($this.attr('min')),
+    max = Number($this.attr('max'))
+  if (min == 0) {
+    var d = 0
+  } else d = min
+  $(qty).on('click', function(){
+    if ($(this).hasClass('minus')) {
+      if (d > min) d += -1
+    } else if ($(this).hasClass('plus')) {
+      var x = Number($this.val()) + 1
+      if (x <= max) d += 1
+    }
+    $this.attr('value', d).val(d)
+  })
+});
 
-for(var i = 0 ; i < price_product.length ; i++)
-{
-  minus_product[i].onclick = function(){ quantity_product[i].innerHTML = quantity_product[i].value - 1  ; price_product[i].innerHTML =  price_default[i].value * quantity_product[i].value  };
-  plus_product[i].onclick = function(){ quantity_product[i].innerHTML = quantity_product[i].value + 1  ; price_product[i].innerHTML =  price_default[i].value * quantity_product[i].value  };
+let sum = 0;
+let quantity = document.querySelectorAll(".input-qty");
+let price = document.querySelectorAll(".price_product_default");
+let changes = document.querySelectorAll(".plus ");
+let inner = document.querySelectorAll(".price_product_on_cart");
+// for (let i = 0; i < quantity.length; i++) {
+//   arr[i] = quantity[i].value;
+// }
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+for(let a = 0; a < changes.length; a++){
+  changes[a].onclick = function() {
+    let result = changes[a].parentElement.children
+    // console.log(result);
+    // console.log(result.length);
+    for(let b = 0; b < result.length; b++){
+      // console.log(result[b].getAttribute("class") === 'input-qty')
+      // console.log(result[b].getAttribute("class"))
+      // console.log(result[a])
+      for (let index = 0; index < inner.length; index++) {
+        if((result[b].getAttribute("class") === 'input-qty')===true && index === a){
+          inner[index].innerHTML = `${Number(result[b].value)*Number(price[index].value)}`;
+          
+        } 
+      }   
+  } 
+  
+  
 }
+}
+
+// let inners = document.querySelectorAll(".price_product_on_cart");
+// for(let i = 0; i < inners.length; i++){
+//   sum += Number(inners[i].textContent);
+//   console.log(inners[i].textContent);
+//   console.log(sum);
+// }
