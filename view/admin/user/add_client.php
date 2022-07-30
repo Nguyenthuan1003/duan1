14e639d19e8737b0c43eeb9560bde8f0d86061cf
nguyenquangminh2001abc <106154144+nguyenquangminh2001abc@users.noreply.github.com>
2022-07-30 10:21:55 -0700

    Update add_client.php

--- view/admin/user/add_client.php
+++ view/admin/user/add_client.php
@@ -5,47 +5,56 @@
             <div class="add-client-1">
                 <button class="btn btn-primary">add khách hàng</button>
             </div>
-            <form action="client.php" method="post">
+            <div>
+                <span>
+                    <?= isset($mesages) ? $mesages : "" ?>
+                </span>
+            </div>
+            <form action="" method="post">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label for="">mã khách hàng</label>
-                            <input type="text" class="form-control" name="id">
-                        </div>
-                        <div class="form-group">
                             <label for="">tên khách hàng</label>
-                            <input type="text" class="form-control" name="name">
+                            <input type="text" class="form-control" name="fullname_user">
+                            <p class="text-danger mt-2"><?= (!empty($_err_add_khach_hang['fullname_user'])) ?  $_err_add_khach_hang['fullname_user'] : "" ?></p>
                         </div>
                         <div class="form-group">
                             <label for="">tên đăng nhập</label>
                             <input type="text" class="form-control" name="name-login">
+                            <p class="text-danger mt-2"><?= (!empty($_err_add_khach_hang['name-login'])) ? $_err_add_khach_hang['name-login']:"" ?></p>
                         </div>
                         <div class="form-group">
                             <label for="">email</label>
                             <input type="text" class="form-control" name="email_user">
-                        </div>
-                        <div class="form-group">
-                            <label for="">số dư tài koản</label>
-                            <input type="text" class="form-control" name="accont_balance">
+                            <p class="text-danger mt-2"><?= (!empty($_err_add_khach_hang['email_user'])) ? $_err_add_khach_hang['email_user']:"" ?></p>
+
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="">số điện thoại</label>
                             <input type="text" class="form-control" name="phone">
+                            <p class="text-danger mt-2"><?= (!empty($_err_add_khach_hang['phone'])) ? $_err_add_khach_hang['phone']:"" ?></p>
                         </div>
                         <div class="form-group">
                             <label for="">mật khẩu </label>
                             <input type="text" class="form-control" name="password">
+                            <p class="text-danger mt-2"><?= (!empty($_err_add_khach_hang['password'])) ? $_err_add_khach_hang['password']:"" ?></p>
                         </div>
 
                         <div class="form-group">
                             <label for="">vai trò</label>
-                            <input type="text" class="form-control" name="role">
+                            <select name="role" id="">
+                                <option value="" hidden></option>
+                                <option value="0">Admin</option>
+                                <option value="1">khách hàng</option>
+                            </select>
+                            <p class="text-danger mt-2"><?= (!empty($_err_add_khach_hang['role'])) ? $_err_add_khach_hang['role']:"" ?></p>
                         </div>
                         <div class="form-group">
                             <label for="">ngày đăng kí</label>
                             <input type="date" class="form-control" name="created_date_user">
+                            <p class="text-danger mt-2"><?= (!empty($_err_add_khach_hang['created_date_user'])) ? $_err_add_khach_hang['created_date_user']:"" ?></p>
                         </div>
 
                     </div>
@@ -53,7 +62,7 @@
                     <div class="button-add-client-2">
 
                         <a href="">
-                            <button type="submit" class="btn btn-outline-primary">Thêm</button>
+                            <button type="submit" name="add_khach_hang" class="btn btn-outline-primary">Thêm</button>
                         </a>
 
                         <a href="">
