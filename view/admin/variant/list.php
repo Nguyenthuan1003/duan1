a240b8c50dd2cdb34030f95ca55b194397c27e05
nguyenquangminh2001abc <minhnqph19074@fpt.edu.vn>
2022-08-08 20:38:13 +0700

    Create list.php

---
+++ view/admin/variant/list.php
@@ -0,0 +1,78 @@
+<main>
+<div class="container-admin">
+        <div class="main">
+            <div class="main-top">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="show-main-top">
+                            <span>show</span>
+                            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example"
+                                id="show-number">
+                                <option selected>10</option>
+                                <option value="1">9</option>
+                                <option value="2">8</option>
+                                <option value="3">7</option>
+                            </select>
+                            <div class="seach-main-top">
+                                <input type="text" class="form-control" placeholder="seach">
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="col-md-6">
+                        <div class="" id="add-row">
+                            <a href="index.php?id_menu=aad_variant">
+                                <button type="button" class="btn btn-primary">+ Thêm Biến Thể</button>
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="main-buttom">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Biến thể</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($variant as $var) : ?>
+                        <tr>
+                            <td scope="row"><?=$var['id_variant']?></td>
+                            <td scope="row"><?=$var['version_variant']?> <?=$var['color_variant']?></td>
+                            <td>
+                                <a href="index.php?id_menu=edit_variant&id=<?=$var['id_variant']?>">
+                                    <button class="btn btn-danger" type="submit">
+                                        <i class="fa-solid fa-pen"></i>
+                                    </button>
+                                </a>
+                                <a href="index.php?id_menu=delete_variant&id=<?=$var['id_variant']?>">
+                                    <button class="btn btn-danger" type="remove">
+                                        <i class="fa-solid fa-trash-can"></i>
+                                    </button>
+                                </a>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+                <div class="next-page">
+                    <button type="button" class="btn btn-light">previous</button>
+                    <button type="button" class="btn btn-primary">1</button>
+                    <button type="button" class="btn btn-primary">2</button>
+                    <button type="button" class="btn btn-primary">3</button>
+                    <button type="button" class="btn btn-light">next</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
