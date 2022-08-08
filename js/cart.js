725937d8ec891dc0ed18a166bc6cac6b336bae80
nguyenquangminh2001abc <minhnqph19074@fpt.edu.vn>
2022-08-08 20:37:22 +0700

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,15 @@
-window.onload = () =>{
+
 $('input.input-qty').each(function() {
   var $this = $(this),
     qty = $this.parent().find('.is-form'),
-    min = Number($this.attr('min')),
+    min = Number($this.attr('mix')),
     max = Number($this.attr('max'))
     if (min == 0) {
     var d = 0
   } else d = min
   $(qty).on('click', function(){
     if ($(this).hasClass('minus')) {
-      if (d > min) d += -1
+      if (d > 1) d += -1
     } else if ($(this).hasClass('plus')) {
       var x = Number($this.val()) + 1
       if (x <= max) d += 1
@@ -17,6 +17,7 @@ $('input.input-qty').each(function() {
     $this.attr('value', d).val(d)
   })
 });
+
 
 let sum = 0;
 let quantity = document.querySelectorAll(".input-qty");
@@ -28,8 +29,8 @@ let money = document.querySelector("#money");
 let showMoney = document.querySelector('#showMoney');
 for(let i = 0; i < inner.length; i++){
   sum += Number(inner[i].textContent);
-  money.innerHTML = `Tiền hàng: ${sum} đ`;
-  showMoney.innerHTML = `Tạm tính: ${sum} đ`;
+  money.innerHTML = `Tạm tính: ${sum.toLocaleString() } đ`;
+  showMoney.innerHTML = `Tiền hàng: ${sum.toLocaleString()} đ`; 
   // console.log(sum)
 }
 for(let a = 0; a < changes.length; a++){
@@ -46,8 +47,8 @@ for(let a = 0; a < changes.length; a++){
   sum = 0;
   for(let i = 0; i < inner.length; i++){
       sum += Number(inner[i].textContent);
-      money.innerHTML = `Tiền hàng: ${sum} đ`;
-      showMoney.innerHTML = `Tạm tính: ${sum} đ`;
+      money.innerHTML = `Tạm tính: ${sum.toLocaleString() } đ`;
+      showMoney.innerHTML = `Tiền hàng: ${sum.toLocaleString()} đ`; 
       // console.log(sum)
   }
 }
@@ -67,8 +68,8 @@ for(let a = 0; a < minus.length; a++){
   sum = 0;
   for(let i = 0; i < inner.length; i++){
       sum += Number(inner[i].textContent);
-      money.innerHTML = `Tiền hàng: ${sum} đ`;
-      showMoney.innerHTML = `Tạm tính: ${sum} đ`;
+      money.innerHTML = `Tạm tính: ${sum.toLocaleString() } đ`;
+      showMoney.innerHTML = `Tiền hàng: ${sum.toLocaleString()} đ`; 
   }
 }
 }
@@ -86,12 +87,11 @@ for(let a = 0; a < quantity.length; a++){
   sum = 0;
   for(let i = 0; i < inner.length; i++){
       sum += Number(inner[i].textContent);
-      money.innerHTML = `Tiền hàng: ${sum} đ`;
-      showMoney.innerHTML = `Tạm tính: ${sum} đ`; 
+      money.innerHTML = `Tạm tính: ${sum.toLocaleString() } đ`;
+      showMoney.innerHTML = `Tiền hàng: ${sum.toLocaleString()} đ`; 
       
   }
 }
 }
-console.log(inner.textContent)
 
-}
+
