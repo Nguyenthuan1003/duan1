edcdce1671927ffad4942ec9e052e6919308758e
nguyenquangminh2001abc <minhnqph19074@fpt.edu.vn>
2022-08-08 20:37:35 +0700

    Update sanpham.php

--- model/sanpham.php
+++ model/sanpham.php
@@ -30,11 +30,7 @@
     }
     function load_one_pro($id){
         $sql = "SELECT * FROM products WHERE id_pro=".$id;
-        return pdo_query($sql);
-    }
-    function select_all_product(){
-        $sql = "SELECT * FROM products";
-        return pdo_query($sql);
+        return pdo_query_one($sql);
     }
     function insert_pro($name, $price, $status, $category, $date, $des_pro, $targets){
         $sql = "INSERT INTO `products`(`pro_name`, `created_date`, `description`, `status`, `id_cate`, `price_default`, `images_default`)
@@ -45,5 +41,74 @@
         $sql = "SELECT * FROM `products` WHERE pro_name='$name'";
         // $sql = "SELECT * FROM `products` WHERE pro_name='aaa'";
         return pdo_query_one($sql);
+    }
+    function select_all_pro(){
+        $sql = "SELECT * FROM `products`";
+        return pdo_query($sql);
+    }
+    function update_pro($name,$created_date,$description,$status,$id_cate,$price_default,$images_default,$id){
+        if($images_default == '' && $created_date == ''){
+            $sql = "UPDATE `products` SET `pro_name`='".$name."',
+            `description`='".$description."',`status`='".$status."',`id_cate`='".$id_cate."',
+            `price_default`='".$price_default."' WHERE id_pro=".$id;
+        }
+        else if($images_default == ''){
+            $sql = "UPDATE `products` SET `pro_name`='".$name."',`created_date`='".$created_date."',
+            `description`='".$description."',`status`='".$status."',`id_cate`='".$id_cate."',
+            `price_default`='".$price_default."' WHERE id_pro=".$id;
+        }
+        else if($created_date == ''){
+            $sql = "UPDATE `products` SET `pro_name`='".$name."',
+            `description`='".$description."',`status`='".$status."',`id_cate`='".$id_cate."',
+            `price_default`='".$price_default."',`images_default`='".$images_default."' WHERE id_pro=".$id;
+        }else{
+            $sql = "UPDATE `products` SET `pro_name`='".$name."',`created_date`='".$created_date."',
+            `description`='".$description."',`status`='".$status."',`id_cate`='".$id_cate."',
+            `price_default`='".$price_default."',`images_default`='".$images_default."' WHERE id_pro=".$id;
+        }
+        pdo_execute($sql);
+    }
+    function delete_pro($id){
+        $sql  = "DELETE FROM `products` WHERE id_pro=".$id;
+        pdo_execute($sql);
+    }
+    function select_all_pro_var(){
+        $sql = "SELECT * FROM `products_attribute` as p JOIN products as pr ON p.id_pro = pr.id_pro 
+        JOIN variant as v ON v.id_variant = p.id_variant";
+        return pdo_query($sql);
+    }
+    function select_one_var($id,$id_var){
+        $sql = "SELECT * FROM `products_attribute` WHERE id_pro='".$id."' AND id_variant='".$id_var."'";
+        // echo $sql;die;
+        return pdo_query_one($sql);
+    }
+    function update_pro_var($id_pro,$id_var,$quantity,$price,$sale,$spec){
+        $sql = "UPDATE `products_attribute` SET 
+        `quantity`='".$quantity."',`price`='".$price."',`sale`='".$sale."',
+        `special_features`='".$spec."' WHERE id_pro='".$id_pro."' AND id_variant=".$id_var;
+        pdo_execute($sql);
+    }
+    function update_img_var($id_pro,$id_var,$image){
+        $sql = "UPDATE `images_products_attribute` SET
+        `images_pro_attri`='".$image."' WHERE id_pro='".$id_pro."' AND id_variant=".$id_var;
+        pdo_execute($sql);
+    }
+    function delete_pro_var($p,$v){
+        $sql = "DELETE FROM `images_products_attribute` WHERE id_pro='".$p."' AND id_variant=".$v;
+        pdo_execute($sql);
+        $sql = "DELETE FROM `products_attribute` WHERE id_pro='".$p."' AND id_variant=".$v;
+        pdo_execute($sql);
+    }
+    function select_sp($keyword,$iddm){
+        $sql = "SELECT * FROM products where 1";
+        if($keyword!=""){
+            $sql.=" and pro_name like '%".$keyword."%'";
+        }
+        if($iddm > 0){
+            $sql.=" and id_cate='".$iddm."'";
+        }
+        
+        $sql.=" ORDER BY id_pro DESC";
+        return pdo_query($sql);
     }
 ?>
