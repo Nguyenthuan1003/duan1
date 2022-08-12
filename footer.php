fd2447d8f208bede370ba22811e4835a98f40d52
nguyenquangminh2001abc <minhnqph19074@fpt.edu.vn>
2022-08-12 22:49:34 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,65 +1,88 @@
-<footer class="bg-dark p-5">
-    <div class="container-fluid">
-        <div class="footer-top">
-            <div class="row">
-                <div class="col-md-3">
-                    <h4 class="text-light p-1">Tổng đài</h4>
-                    <a href="" class="link-ft">
-                        <p class="text-light p-1">Mua hàng: 1900.9696.42 (7:30 - 22:00)</p>
-                    </a><a href="" class="link-ft">
-                        <p class="text-light p-1">CSKH: 1900.9868.43 (8:00 - 21:30)</p>
-                    </a><a href="" class="link-ft">
-                        <p class="text-light p-1">Kỹ thuật: 1900.8668.54 (7:30 - 22:00)</p>
+<script defer="defer" async="async"
+        src="//cdn.tgdd.vn/mwgcart/topzone/js/bundle/detail.min.v202208080700.js"></script>
+    
+
+<footer>
+        <div class="foot">
+            <div class="logo-foot">
+            </div>
+            <ul class="list-foot">
+                <li>
+                    <span>Tổng đài</span>
+                    <a href="tel:1900969642">
+                        <span>Mua hàng:</span>
+                        <b>1900.9696.42</b> (7:30 - 22:00)
                     </a>
-                </div>
-                <div class="col-md-3">
-                    <h4 class="text-light p-1">Hệ thống cửa hàng</h4>
-                    <a href="" class="link-ft">
-                        <p class="text-light p-1">Nội quy cửa hàng</p>
-                    </a><a href="" class="link-ft">
-                        <p class="text-light p-1">Chất lượng phục vụ</p>
-                    </a><a href="" class="link-ft">
-                        <p class="text-light p-1">Chính sách bảo hành & đổi trả</p>
+                    <a href="tel:1900986843">
+                        <span>CSKH:</span>
+                        <b>1900.9868.43</b> (8:00 - 21:30)
                     </a>
-                </div>
-                <div class="col-md-3">
-                    <h4 class="text-light p-1">Hỗ trợ khách hàng</h4>
-                    <a href="" class="link-ft">
-                        <p class="text-light p-1">Điều kiện giao dịch chung</p>
-                    </a><a href="" class="link-ft">
-                        <p class="text-light p-1">Hướng dẫn mua hàng online</p>
-                    </a><a href="" class="link-ft">
-                        <p class="text-light p-1">Chính sách giao hàng</p>
-                    </a><a href="" class="link-ft">
-                        <p class="text-light p-1">Hướng dẫn thanh toán</p>
+                    <a href="tel:1900866854">
+                        <span>Kỹ thuật:</span>
+                        <b>1900.8668.54</b> (7:30 - 22:00)
                     </a>
-                </div>
-                <div class="col-md-3">
-                    <h4 class="text-light p-1">Thương hiệu</h4>
-                    <a href="" class="link-ft">
-                        <p class="text-light p-1">Hướng dẫn mua hàng online</p>
-                    </a><a href="" class="link-ft">
-                        <p class="text-light p-1">Chính sách giao hàng</p>
-                    </a><a href="" class="link-ft">
-                        <p class="text-light p-1">Hướng dẫn thanh toán</p>
+                </li>
+                <li>
+                    <span>Hệ thống cửa hàng</span>
+    
+                    <a href="">
+                        Nội quy cửa hàng
                     </a>
-                </div>
+                    <a href="">
+                        Chất lượng phục vụ
+                    </a>
+                    <a href="">
+                        Chính sách bảo hành & đổi trả
+                    </a>
+                </li>
+                <li>
+                    <span>Hỗ trợ khách hàng</span>
+                    <a href="">
+                        Điều kiện giao dịch chung
+                    </a>
+                    <a href="">
+                        Hướng dẫn mua hàng online
+                    </a>
+                    <a href="">
+                        Chính sách giao hàng
+                    </a>
+                    <a href="">
+                        Hướng dẫn thanh toán
+                    </a>
+                </li>
+                <li>
+                    <span>Về Thế Giới Alo</span>
+
+                    <a href=""
+                        class="color-link">
+                        Tích điểm Quà tặng VIP
+                    </a>
+                    <a href="">
+                        Chính sách bảo mật thông tin
+                    </a>
+                    <a rel="" href="">Xem
+                        bản mobile</a>
+                </li>
+            </ul>
+            <div class="text-cpr">
+                <p>
+                    © 2022. Công ty cổ phần Thế Giới ALO. <br />
+                    Địa chỉ: 128 Trần Quang Khải, P. Tân Định, Q.1, TP. Hồ Chí Minh. Điện thoại: 028 38125960.
+                </p>
+            </div>
+            <div class="certify">
+                <a href="" target="_blank"><img class="lazyload"
+                        data-src="//cdn.tgdd.vn/mwgcart/topzone/images/certify-bct.png" width="127" /></a>
+                <a href=""
+                    title="Chung nhan Tin Nhiem Mang" target="_blank"><img class="lazyload"
+                        data-src="https://tinnhiemmang.vn/handle_cert?id=topzone.vn" width="150"
+                        alt="Chung nhan Tin Nhiem Mang"></a>
+                <a href="" target="_blank"
+                    title="DMCA.com Protection Status" class="dmca-badge">
+                    <img class="lazyload"
+                        data-src="https://images.dmca.com/Badges/_dmca_premi_badge_4.png?ID=4f44c8e7-b645-4ddb-8aec-c130d0598c85"
+                        alt="DMCA.com Protection Status" />
+                </a>
             </div>
         </div>
-        <div class="footer-end">
-            <div class="row">
-                <div class="col-md-9">
-                    <p class="text-light p-1">© 2018. Công ty cổ phần Thế Giới Alo. GPDKKD: 0303217354 do sở KH & ĐT TP.HN cấp ngày 02/01/2022</p>
-                    <p class="text-light p-1">Địa chỉ: 128 Thanh Xuân - Hà Nội . Điện thoại: 028 38125960.</p>
-                </div>
-                <div class="col-md-3">
-                    <img src="./image/image 1.png" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-</body>
-</html>
+    </footer>
